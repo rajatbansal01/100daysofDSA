341e345ad61db2b01571e0e2d691f4a3f9bf6a49
Rajat Bansal <65078224+rajatbansal01@users.noreply.github.com>
2021-03-07 13:54:35 +0530

    day 6

---
+++ day 6/delete_node.py
@@ -0,0 +1,71 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+    
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+
+
+    def print_llist(self):
+        temp = self.head
+        while temp:
+            print(temp.data)
+            temp=temp.next
+
+    def push(self,new_data):
+        new_node = Node(new_data)
+        new_node.next=self.head
+        self.head =new_node
+            
+    def append(self,new_data):
+        new_node = Node(new_data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        last = self.head
+        while(last.next):
+            last=last.next
+        last.next=new_node
+
+    def delete_node(self,key):
+        temp = self.head
+
+        if (temp is not None):
+            if (temp.data  == key):
+                self.head = temp.next
+                temp = None
+                return
+        while temp is not None:
+            if temp.data == key:
+                break
+            prev = temp
+            temp = temp.next
+        if temp==None:
+            return
+        prev.next = temp.next
+        temp = None
+    
+
+
+
+        
+
+llist = LinkedList() 
+
+llist.push(1)
+llist.push(2)
+llist.push(3)
+llist.push(4)
+
+llist.append(11)
+llist.append(12)
+llist.append(13)
+
+
+llist.delete_node(11)
+llist.print_llist() 
