c9869ba5136cd10b84fbfa5810178382e07b7949
Rajat Bansal <65078224+rajatbansal01@users.noreply.github.com>
2021-03-07 13:20:33 +0530

    day 5

---
+++ day_5/insert_node.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+    
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+
+
+    def print_llist(self):
+        temp = self.head
+        while temp:
+            print(temp.data)
+            temp=temp.next
+
+    def push(self,new_data):
+        new_node = Node(new_data)
+        new_node.next=self.head
+        self.head =new_node
+            
+    def append(self,new_data):
+        new_node = Node(new_data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        last = self.head
+        while(last.next):
+            last=last.next
+        last.next=new_node
+
+
+
+        
+
+llist = LinkedList() 
+
+llist.push(1)
+llist.push(2)
+llist.push(3)
+llist.push(4)
+
+llist.append(11)
+llist.append(12)
+llist.append(13)
+
+llist.print_llist() 
