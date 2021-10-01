7adcb3ee457ab3d3a9420efedf059957791f83e1
sonuyadav3 <86907542+sonuyadav3@users.noreply.github.com>
2021-10-01 11:03:14 +0530

    comments added

--- Search Algorithms/Linear_search.py
+++ Search Algorithms/Linear_search.py
@@ -3,6 +3,7 @@ def linear_search(arr,x):
         if arr[i]==x:
             return(i)
     return("not found")
-
+#test case
 arr=[2,3,4,5,6,7,45]
+#call the function and print
 print(linear_search(arr,4))
