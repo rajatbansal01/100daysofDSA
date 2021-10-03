0a75240663df04f42865254abbfac408032a4aca
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2021-10-03 23:45:11 +0530

    Update templateForCP.cpp

--- C++/templateForCP.cpp
+++ C++/templateForCP.cpp
@@ -1,1 +1,24 @@
+#include<bits/stdc++.h>
+#pragma GCC optimise("Ofast")
+#pragma GCC target("avx","avx2","fma")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+using namespace std;
+#define int long long
+
+void solve(){
+
+}
+
+signed main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
 
