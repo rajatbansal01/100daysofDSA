1705fcdb0cde0b0341730f36e5214459ad49655d
Dan Tcheche <dtchechenistky@devartis.com>
2021-10-04 09:38:08 -0300

    Add Encapsulation

--- Python OOPs/inheritance.py
+++ Python OOPs/inheritance.py
@@ -2,9 +2,18 @@ class Vehicle:
 
     def __init__(self, brand, name, color, wheels):
         self.brand = brand
-        self.name = name
-        self.color = color
+
+        #   Encapsulation protected variable, Vehicle, Car and Motorbike can access this variable.
+        self._name = name
+
+        #   Encapsulation private variable, only Vehicle class can access this variable.
+        self.__color = color
         self.wheels = wheels
+
+    #   A way to expose a private variable so it can be read by others but it still can't be modified
+    #   from outside the class.
+    def get_color(self):
+        return self.__color
 
 
 class Car(Vehicle):
--- Python OOPs/readme.md
+++ Python OOPs/readme.md
@@ -17,3 +17,11 @@ when instantiating an object.
 ##Inheritance:
 * Inheritance is the mechanism of basing an object or class upon another object or class,
 retaining similar implementation.
+
+##Encapsulation:
+* Encapsulation describes the idea of bundling data and methods that 
+work on that data within a class. This concept is also often used
+to hide the internal representation, or state, of an object from the outside.
+The main idea is to protect data from being modified from outside the class.
+* A protected variable can only be accessed by the parent and child classes.
+* A private variable can only be accessed through the class itself.
