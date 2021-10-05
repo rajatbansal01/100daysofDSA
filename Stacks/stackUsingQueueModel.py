739a29dcc8a5d43d4a3969e22d853c045c6f60ad
Amit Kumar <amit.ak.ak933@gmail.com>
2021-10-05 13:26:34 +0530

    implementation of Stacks using queue model added

---
+++ Stacks/stackUsingQueueModel.py
@@ -0,0 +1,31 @@
+#implementing stack using Queue model
+
+
+from queue import LifoQueue
+
+# Initializing a stack
+stack = LifoQueue(maxsize = 3)
+
+# qsize() show the number of elements
+# in the stack
+print(stack.qsize())
+
+# put() function to push
+# element in the stack
+stack.put('a')
+stack.put('b')
+stack.put('c')
+stack.put('d')
+stack.put('e')
+
+print("Full: ", stack.full())
+print("Size: ", stack.qsize())
+
+# get() function to pop element from stack in
+# LIFO order
+print('\nElements popped from the stack')
+print(stack.get())
+print(stack.get())
+print(stack.get())
+
+print("\nEmpty: ", stack.empty())
