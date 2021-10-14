0b05840a116d09e4235c4ac53685471ce7c1c146
Rajat Bansal <rajatbansal884@gmail.com>
2021-10-14 12:40:36 +0530

    Update replace_pi.py

--- Recursion/replace_pi.py
+++ Recursion/replace_pi.py
@@ -1,3 +1,23 @@
+"""
+Replace pi with 3.14
+Given a string s and replace all occurances of pi with 3.14.
+
+l = len(s)
+
+Approach
+Base Case:
+
+if l == 0 or l == 1 then return the s as it is.
+
+Induction Step and Induction Hypothesis:
+
+Will be true for next part of string as well. There are two cases.
+
+Case 1: if first two characters of s are 'pi' then return replacePi(s[2:])+'3.14'.
+
+Case 2: if first two chars are not pi then return return replacePi(s[1:])+s[0].
+"""
+
 def replacepi(s):
     if len(s)==0 or len(s)==1:
         return s
@@ -8,3 +28,8 @@ def replacepi(s):
         smalloutput=replacepi(s[1:])
         return s[0]+smalloutput
 print(replacepi('pi*r*r'))
+## output = 3.14*r*r
+
+# test 2
+print(replacepi('hellopipipir'))
+## output = 'hello3.143.143.14r'
