543e80aa0c30c10f6424cc141a56dfd58d5fd703
Rajat <rajatbansal884@gmail.com>
2022-02-27 16:57:08 +0530

    started game

--- CN DSA/logics.py
+++ CN DSA/logics.py
@@ -1,12 +1,66 @@
-def start_game():
-    mat = []
-    for i in range(4):
-        mat.append([0]*4)
-    return mat
+# import module
+import os
+
+# function to establish a new connection
 
 
-def print_mat(mat):
-    for i in range(4):
-        for j in range(4):
-            print(mat[i][j], end=' ')
-        print()
+def createNewConnection(name, SSID, password):
+    config = """<?xml version=\"1.0\"?>
+<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+	<name>"""+name+"""</name>
+	<SSIDConfig>
+		<SSID>
+			<name>"""+SSID+"""</name>
+		</SSID>
+	</SSIDConfig>
+	<connectionType>ESS</connectionType>
+	<connectionMode>auto</connectionMode>
+	<MSM>
+		<security>
+			<authEncryption>
+				<authentication>WPA2PSK</authentication>
+				<encryption>AES</encryption>
+				<useOneX>false</useOneX>
+			</authEncryption>
+			<sharedKey>
+				<keyType>passPhrase</keyType>
+				<protected>false</protected>
+				<keyMaterial>"""+password+"""</keyMaterial>
+			</sharedKey>
+		</security>
+	</MSM>
+</WLANProfile>"""
+    command = "netsh wlan add profile filename=\""+name+".xml\""+" interface=Wi-Fi"
+    with open(name+".xml", 'w') as file:
+        file.write(config)
+    os.system(command)
+
+# function to connect to a network
+
+
+def connect(name, SSID):
+    command = "netsh wlan connect name=\""+name + \
+        "\" ssid=\""+SSID+"\" interface=Wi-Fi"
+    os.system(command)
+
+# function to display avavilabe Wifi networks
+
+
+def displayAvailableNetworks():
+    command = "netsh wlan show networks interface=Wi-Fi"
+    os.system(command)
+
+
+# display available netwroks
+displayAvailableNetworks()
+
+# input wifi name and password
+name = "AGGWAR 2.4"
+password = "16161616"
+
+# establish new connection
+createNewConnection(name, name, password)
+
+# connect to the wifi network
+connect(name, name)
+print("If you aren't connected to this network, try connecting with the correct password!")
