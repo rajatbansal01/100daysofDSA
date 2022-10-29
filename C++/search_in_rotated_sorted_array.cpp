97db33593f65168f6e70f34a46e0894fdef95b30
Nishkarsh1199 <85515355+Nishkarsh1199@users.noreply.github.com>
2022-10-29 13:31:08 +0530

    Create search_in_rotated_sorted_array.cpp

---
+++ C++/search_in_rotated_sorted_array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        
+        int s=0; int e=n-1;
+        int mid;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target)
+                return mid;
+            if(nums[s]<=nums[mid]){
+                if(target<=nums[mid] && target>=nums[s])
+                    e=mid-1;
+                else
+                    s=mid+1;
+            }
+            else
+                if(target>=nums[mid] && target<=nums[e])
+                    s=mid+1;
+            else
+                e=mid-1;
+        }
+        return -1;
+    }
