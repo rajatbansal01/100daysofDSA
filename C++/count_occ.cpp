df9cde76e0571786f97f0763c8431df54abc1ce1
Aastha <118296087+AASTHA9416@users.noreply.github.com>
2023-10-31 21:45:10 +0530

    added changes

--- C++/count_occ.cpp
+++ C++/count_occ.cpp
@@ -4,8 +4,16 @@
 // there are two approch sipmle one and mapmethod
 #include<iostream>
 using namespace std;
-
+int unique_ele(int arr[],int n){
+    int ans=0;
+for(int i=0;i<n;i++){
+ans=ans^arr[i];
+}
+return ans;
+}
 int main(){
-    
+    int arr[5]={2,2,3,4,4};
+int k=unique_ele(arr,5);
+cout<<k;
     return 0;
 }
